7454f51647cdf1804d8b0912cf16e8a12fbdbfbc
Mitsuharu Emoto <emoto@seesaa.co.jp>
2018-12-04 09:06:13 +0900

    Initial Commit

---
+++ MEOKit/MEOKit.h
@@ -0,0 +1,19 @@
+//
+//  MEOKit.h
+//  MEOKit
+//
+//  Created by Mitsuharu Emoto on 2018/12/04.
+//  Copyright © 2018 Mitsuharu Emoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MEOKit.
+FOUNDATION_EXPORT double MEOKitVersionNumber;
+
+//! Project version string for MEOKit.
+FOUNDATION_EXPORT const unsigned char MEOKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MEOKit/PublicHeader.h>
+
+
