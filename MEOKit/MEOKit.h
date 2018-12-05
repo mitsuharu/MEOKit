7b65eb461527a353d1e56eb9a93141a26515da0f
Mitsuharu Emoto <emoto@seesaa.co.jp>
2018-12-05 09:03:39 +0900

    fix CommonCrypto

--- MEOKit/MEOKit.h
+++ MEOKit/MEOKit.h
@@ -16,6 +16,3 @@ FOUNDATION_EXPORT const unsigned char MEOKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MEOKit/PublicHeader.h>
 
-
-// bridging-header
-#import "CommonCrypto/CommonHMAC.h"
--- MEOKit/extensions/String+.swift
+++ MEOKit/extensions/String+.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CommonCrypto
 
 extension String {
 
