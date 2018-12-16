cea3bfcd0e38e850e0944f978457ed092d6dcf86
Mitsuhau Emoto <>
2018-12-16 21:21:22 +0900

    no message

--- MEOKit.podspec
+++ MEOKit.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "MEOKit"
-  s.version      = "0.1.2"
+  s.version      = "0.1.3"
   s.summary      = "my libraries"
 
   s.description  = <<-DESC
--- MEOKit/extensions/UIColor+.swift
+++ MEOKit/extensions/UIColor+.swift
@@ -8,24 +8,26 @@
 
 import UIKit
 
+public func RGBA(_ r:Int, _ g:Int, _ b:Int, _ a:CGFloat) -> UIColor{
+    return UIColor(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0,
+                   blue: CGFloat(b)/255.0, alpha: a)
+}
+
+public func RGB(_ r:Int, _ g:Int, _ b:Int) -> UIColor{
+    return RGBA(r, g, b, 1.0)
+}
+
+public func HEXCOLOR(_ hexCode: String)  -> UIColor{
+    return UIColor(hexCode: hexCode)
+}
+
 extension UIColor{
-    
-    /// RBGA(0 - 255)でUIColorを生成する
-    private convenience init(r:Int, g:Int, b:Int, a:CGFloat=1.0) {
-        self.init(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0,
-                  blue: CGFloat(b)/255.0, alpha: a)
-    }
     
     /// 16進数でUIColorを生成する
     public convenience init(hexCode: String, a: CGFloat = 1.0) {
         let (r, g, b) = UIColor.hexCode2RGB(hexCode: hexCode)
         self.init(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0,
                   blue: CGFloat(b)/255.0, alpha: a)
-    }
-    
-    /// RBGAでUIColorを生成する
-    public static func RGBA(r:Int, g:Int, b:Int, a:CGFloat=1.0) -> UIColor{
-        return UIColor(r: r, g: g, b: b, a: a)
     }
     
     /// 16進数でUIColorを生成する
